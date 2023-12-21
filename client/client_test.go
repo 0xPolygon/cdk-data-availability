697942ad01c59e2b470e0fbb05364a612895eeb1
begmaroman <begmaroman@gmail.com>
2023-12-21 11:06:43 +0000

    Added tests

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"context"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
@@ -8,6 +9,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/0xPolygon/cdk-data-availability/rpc"
 	"github.com/0xPolygon/cdk-data-availability/types"
@@ -40,6 +43,14 @@ func TestClient_SignSequence(t *testing.T) {
 			result: `{"error":{"code":123,"message":"test error"}}`,
 			err:    errors.New("123 test error"),
 		},
+		{
+			name: "invalid signature returned by server",
+			ss: types.SignedSequence{
+				Sequence:  types.Sequence{},
+				Signature: []byte("signature0"),
+			},
+			result: `{"result":"invalid-signature"}`,
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
@@ -50,6 +61,7 @@ func TestClient_SignSequence(t *testing.T) {
 			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				var res rpc.Request
 				require.NoError(t, json.NewDecoder(r.Body).Decode(&res))
+				require.Equal(t, "datacom_signSequence", res.Method)
 
 				var params []types.SignedSequence
 				require.NoError(t, json.Unmarshal(res.Params, &params))
@@ -64,10 +76,71 @@ func TestClient_SignSequence(t *testing.T) {
 
 			got, err := c.SignSequence(tt.ss)
 			if tt.err != nil {
+				require.Error(t, err)
 				require.EqualError(t, tt.err, err.Error())
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, tt.signature, got)
+			}
+		})
+	}
+}
+
+func TestClient_GetOffChainData(t *testing.T) {
+	tests := []struct {
+		name   string
+		hash   common.Hash
+		result string
+		data   []byte
+		err    error
+	}{
+		{
+			name:   "successfully got offhcain data",
+			hash:   common.BytesToHash([]byte("hash")),
+			result: fmt.Sprintf(`{"result":"%s"}`, hex.EncodeToString([]byte("offchaindata"))),
+			data:   []byte("offchaindata"),
+		},
+		{
+			name:   "error returned by server",
+			hash:   common.BytesToHash([]byte("hash")),
+			result: `{"error":{"code":123,"message":"test error"}}`,
+			err:    errors.New("123 test error"),
+		},
+		{
+			name:   "invalid offchain data returned by server",
+			hash:   common.BytesToHash([]byte("hash")),
+			result: `{"result":"invalid-signature"}`,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				var res rpc.Request
+				require.NoError(t, json.NewDecoder(r.Body).Decode(&res))
+				require.Equal(t, "sync_getOffChainData", res.Method)
+
+				var params []common.Hash
+				require.NoError(t, json.Unmarshal(res.Params, &params))
+				require.Equal(t, tt.hash, params[0])
+
+				_, err := fmt.Fprint(w, tt.result)
+				require.NoError(t, err)
+			}))
+			defer svr.Close()
+
+			c := &Client{url: svr.URL}
+
+			got, err := c.GetOffChainData(context.Background(), tt.hash)
+			if tt.err != nil {
+				require.Error(t, err)
+				require.EqualError(t, tt.err, err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.data, got)
 			}
 		})
 	}
