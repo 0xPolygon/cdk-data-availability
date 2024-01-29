1e69fef208df0d31b6ff9b9bd34c2f3d53f9187c
begmaroman <begmaroman@gmail.com>
2024-01-29 14:50:20 +0000

    Added unit tests for IsHexValid function

--- types/types.go
+++ types/types.go
@@ -88,7 +88,7 @@ type ArgHash common.Hash
 
 // UnmarshalText unmarshals from text
 func (arg *ArgHash) UnmarshalText(input []byte) error {
-	if !HexIsValid(string(input)) {
+	if !IsHexValid(string(input)) {
 		return fmt.Errorf("invalid hash, it needs to be a hexadecimal value")
 	}
 
@@ -123,8 +123,8 @@ func decodeToHex(b []byte) ([]byte, error) {
 	return hex.DecodeString(str)
 }
 
-// HexIsValid checks if the provided string is a valid hexadecimal value
-func HexIsValid(s string) bool {
+// IsHexValid checks if the provided string is a valid hexadecimal value
+func IsHexValid(s string) bool {
 	str := strings.TrimPrefix(s, "0x")
 	for _, b := range []byte(str) {
 		if !(b >= '0' && b <= '9' || b >= 'a' && b <= 'f' || b >= 'A' && b <= 'F') {
