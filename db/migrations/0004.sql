0cd4253258deb40a10d4720ff76d844ee2144c5d
begmaroman <begmaroman@gmail.com>
2024-03-08 11:34:04 -0600

    Fixed sonarcube

--- db/migrations/0004.sql
+++ db/migrations/0004.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS data_node.unresolved_batches CASCADE;
 CREATE TABLE data_node.unresolved_batches
 (
     num         BIGINT NOT NULL,
-    hash        VARCHAR NOT NULL,
+    hash        VARCHAR(255) NOT NULL,
     created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
 
     PRIMARY KEY (num, hash)
